c83d6eacf2ca2de91cef42f01933e1f965861cb2
Mettle X <56449790+mettlex@users.noreply.github.com>
2021-05-17 03:32:11 +0600

    move prefix, time & color to a config file

---
+++ src/games/word-chain/config.ts
@@ -0,0 +1,17 @@
+export const prefixes = [
+  "wc.",
+  "wordchain.",
+  "word-chain.",
+  "wc .",
+  "wordchain .",
+  "word-chain .",
+];
+
+export const secondsToJoin = 60;
+export const turnSeconds = [30, 25, 20, 15, 10];
+
+export const flatColors = {
+  red: "#f62459",
+  green: "#00b16a",
+  blue: "#19b5fe",
+};
