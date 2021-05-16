26f9a95a316c45618d15945b5976bcb15f1a3372
Mettle X <56449790+mettlex@users.noreply.github.com>
2021-05-17 03:32:54 +0600

    add halt feature

---
+++ src/games/word-chain/handlers/halt.ts
@@ -0,0 +1,51 @@
+import pino from "pino";
+import { Message, MessageEmbed } from "discord.js";
+import { getAllActiveGames, getCurrentGame } from "..";
+import { flatColors } from "../config";
+
+const logger = pino({ prettyPrint: process.env.NODE_ENV !== "production" });
+
+const haltHanlder = (message: Message) => {
+  if (!message.member?.hasPermission("MANAGE_GUILD")) {
+    const embed = new MessageEmbed();
+
+    embed
+      .setDescription("Only server managers can halt a running game.")
+      .setColor(flatColors.red);
+
+    message.channel.send({ embed, content: `${message.author}` });
+
+    return;
+  }
+
+  const channelId = message.channel.id;
+
+  const currentGame = getCurrentGame(channelId);
+
+  if (!currentGame) {
+    const embed = new MessageEmbed();
+
+    embed.setDescription("There is no running game.").setColor(flatColors.red);
+
+    message.channel.send({ embed, content: `${message.author}` });
+
+    return;
+  }
+
+  const activeGames = getAllActiveGames();
+
+  activeGames[channelId] = undefined;
+
+  const embed = new MessageEmbed();
+
+  embed
+    .setTitle("Word-Chain Game Stopped!")
+    .setDescription(`The running game has been stopped by ${message.author}.`)
+    .setColor(flatColors.green);
+
+  message.channel.send({ embed }).catch((e) => {
+    logger.error(e);
+  });
+};
+
+export default haltHanlder;
