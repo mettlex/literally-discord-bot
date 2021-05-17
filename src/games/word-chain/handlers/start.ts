cba46c4f6209183379d01fc4acb87b9a8b048c50
Mettle X <56449790+mettlex@users.noreply.github.com>
2021-05-18 01:06:21 +0600

    add changing turns for correct words

--- src/games/word-chain/handlers/start.ts
+++ src/games/word-chain/handlers/start.ts
@@ -18,6 +18,9 @@ const changeTurn = async (message: Message, timeLeft?: number) => {
     return;
   }
 
+  const currentGameTurnSeconds =
+    turnSeconds[currentGame.roundIndex] || turnSeconds[turnSeconds.length - 1];
+
   if (currentGame.userIds.length === 1) {
     activeGames[channelId] = undefined;
 
@@ -40,7 +43,10 @@ const changeTurn = async (message: Message, timeLeft?: number) => {
     }
 
     if (currentGame.longestWord) {
-      embed.addField("Longest Word", currentGame.longestWord);
+      embed.addField(
+        "Longest Word",
+        `${currentGame.longestWord} <@${currentGame.longestWordUserId}>`,
+      );
     }
 
     message.channel.send({ embed }).catch((e) => {
@@ -52,7 +58,9 @@ const changeTurn = async (message: Message, timeLeft?: number) => {
 
   if (!timeLeft) {
     const embed1 = new MessageEmbed()
-      .setDescription(`Send a message with a word following the criteria:`)
+      .setDescription(
+        `Send a message with an English word following the criteria:`,
+      )
       .addField(
         "Starting Letter",
         `**${currentGame.currentStartingLetter.toUpperCase()}**`,
@@ -61,7 +69,7 @@ const changeTurn = async (message: Message, timeLeft?: number) => {
         "Minimum Word Length",
         `**${currentGame.currentWordMinLength}** characters`,
       )
-      .addField("Time Left", `**${turnSeconds[0]}** seconds`)
+      .addField("Time Left", `**${currentGameTurnSeconds}** seconds`)
       .setColor(flatColors.blue);
 
     message.channel
@@ -74,7 +82,7 @@ const changeTurn = async (message: Message, timeLeft?: number) => {
   const filter = (m: Message) =>
     m.author.id === currentGame!.currentUser && /^\w/gi.test(m.content);
 
-  const time = timeLeft || turnSeconds[currentGame.roundIndex] * 1000;
+  const time = timeLeft || currentGameTurnSeconds * 1000;
 
   const waitingStartTime = new Date();
 
@@ -90,12 +98,15 @@ const changeTurn = async (message: Message, timeLeft?: number) => {
       .setTitle("Time Out!")
       .setDescription(
         // eslint-disable-next-line max-len
-        `<@${currentGame.currentUser}> couldn't send a word within ${turnSeconds[0]} seconds.`,
+        `<@${currentGame.currentUser}> couldn't send a correct word within ${currentGameTurnSeconds} seconds.`,
       )
       .addField("Eliminated Player", `<@${currentGame.currentUser}>`)
       .setColor(flatColors.red);
 
-    activeGames[channelId]!.userIds.splice(0, 1);
+    activeGames[channelId]!.userIds.splice(
+      currentGame.userIds.indexOf(currentGame.currentUser),
+      1,
+    );
 
     activeGames[channelId] = {
       ...currentGame,
@@ -117,13 +128,34 @@ const changeTurn = async (message: Message, timeLeft?: number) => {
     return;
   }
 
-  if (
-    !currentPlayerMessage.content
-      .toLowerCase()
-      .startsWith(currentGame.currentStartingLetter.toLowerCase()) ||
-    currentPlayerMessage.content.length < currentGame.currentWordMinLength ||
-    !checkSpell(currentPlayerMessage.content.split(" ")[0])
-  ) {
+  const word = currentPlayerMessage.content.split(" ")[0];
+
+  const condition1 = !currentPlayerMessage.content
+    .toLowerCase()
+    .startsWith(currentGame.currentStartingLetter.toLowerCase());
+
+  const condition2 =
+    currentPlayerMessage.content.length < currentGame.currentWordMinLength;
+
+  const condition3 = !checkSpell(word);
+
+  const condition4 = currentGame.usedWords.includes(word.toLowerCase());
+
+  if (condition1 || condition2 || condition3 || condition4) {
+    let reason = `Reason: `;
+
+    if (condition1) {
+      // eslint-disable-next-line max-len
+      reason += `It doesn't start with \`${currentGame.currentStartingLetter.toUpperCase()}\`.`;
+    } else if (condition2) {
+      // eslint-disable-next-line max-len
+      reason += `It doesn't have minimum of ${currentGame.currentWordMinLength} letters.`;
+    } else if (condition3) {
+      reason += `The word isn't recognized by Hunspell dictionary.`;
+    } else if (condition4) {
+      reason += `The word was already used before.`;
+    }
+
     const elapsedTime = differenceInMilliseconds(new Date(), waitingStartTime);
     const newTimeLeft = time - elapsedTime;
 
@@ -132,7 +164,8 @@ const changeTurn = async (message: Message, timeLeft?: number) => {
         `**__${currentPlayerMessage.content
           .split(" ")[0]
           .toUpperCase()}__** is incorrect.
-        \nSend a message with a word following the criteria:`,
+        ${reason}
+        Send a message with a word following the criteria:`,
       )
       .addField(
         "Starting Letter",
@@ -156,7 +189,7 @@ const changeTurn = async (message: Message, timeLeft?: number) => {
         .setTitle("Time Out!")
         .setDescription(
           // eslint-disable-next-line max-len
-          `<@${currentGame.currentUser}> couldn't send a word within ${turnSeconds[0]} seconds.`,
+          `<@${currentGame.currentUser}> couldn't send a word within ${currentGameTurnSeconds} seconds.`,
         )
         .addField("Eliminated Player", `<@${currentGame.currentUser}>`)
         .setColor(flatColors.red);
@@ -184,7 +217,63 @@ const changeTurn = async (message: Message, timeLeft?: number) => {
     }
 
     await changeTurn(message, newTimeLeft);
+
+    return;
   }
+
+  const usedWords = [...activeGames[channelId]!.usedWords, word.toLowerCase()];
+
+  const longestWord =
+    activeGames[channelId]!.longestWord.length < word.length
+      ? word
+      : activeGames[channelId]!.longestWord;
+
+  let longestWordUserId = activeGames[channelId]!.longestWordUserId;
+
+  if (longestWord === word) {
+    longestWordUserId = message.author.id;
+  }
+
+  const currentStartingLetter = word.slice(-1);
+
+  if (
+    currentGame.userIds.indexOf(currentGame.currentUser) ===
+    currentGame.userIds.length - 1
+  ) {
+    const shouldReduce = !activeGames[channelId]!.reduce;
+
+    activeGames[channelId] = {
+      ...activeGames[channelId]!,
+      currentUser: currentGame.userIds[0],
+      longestWord,
+      longestWordUserId,
+      usedWords,
+      currentStartingLetter,
+      currentWordMinLength: Math.min(
+        9,
+        shouldReduce
+          ? activeGames[channelId]!.currentWordMinLength + 1
+          : activeGames[channelId]!.currentWordMinLength,
+      ),
+      roundIndex: shouldReduce
+        ? activeGames[channelId]!.roundIndex + 1
+        : activeGames[channelId]!.roundIndex,
+      reduce: shouldReduce,
+    };
+  } else {
+    const userIndex = currentGame.userIds.indexOf(currentGame.currentUser);
+
+    activeGames[channelId] = {
+      ...activeGames[channelId]!,
+      currentUser: currentGame.userIds[userIndex + 1],
+      longestWord,
+      longestWordUserId,
+      usedWords,
+      currentStartingLetter,
+    };
+  }
+
+  await changeTurn(message);
 };
 
 const startHandler = (message: Message) => {
@@ -202,6 +291,7 @@ const startHandler = (message: Message) => {
     joinable: true,
     userIds: [message.author.id],
     longestWord: "",
+    longestWordUserId: "",
     currentUser: message.author.id,
     currentWordMinLength: 3,
     currentStartingLetter: String.fromCodePoint(
@@ -209,6 +299,8 @@ const startHandler = (message: Message) => {
         "a".charCodeAt(0),
     ),
     roundIndex: 0,
+    usedWords: [],
+    reduce: false,
   };
 
   const tid = setTimeout(async () => {
@@ -264,8 +356,11 @@ const startHandler = (message: Message) => {
   }, secondsToJoin * 1000);
 
   const embed = new MessageEmbed()
-    .setTitle("Starting Word-Chain Game!")
-    .setDescription(`${message.author} has requested you to join the game.`)
+    .setTitle("Join Word-Chain Game!")
+    .setDescription(
+      `${message.author} is starting a word-chain game.
+      You may join to start playing together.`,
+    )
     .addField(
       "How to join",
       `Send \`${prefixes[0]}${actions[2].commands[0]}\` or \`${prefixes[0]}${
@@ -273,7 +368,7 @@ const startHandler = (message: Message) => {
       }\` here in this channel to join`,
     )
     .addField("Time Left", `${secondsToJoin} seconds`)
-    .setColor(flatColors.green);
+    .setColor(flatColors.yellow);
 
   message.channel.send(embed).catch((e) => {
     logger.error(e);
