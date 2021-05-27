9f0128be1d39e320a3b1d3c9b93b7f132a368358
Mettle X <56449790+mettlex@users.noreply.github.com>
2021-05-27 14:56:47 +0600

    update word-chain game prefix

--- src/games/word-chain/config.ts
+++ src/games/word-chain/config.ts
@@ -1,12 +1,4 @@
-export const prefixes = [
-  "wc.",
-  "wc/",
-  "wordchain.",
-  "word-chain.",
-  "wc .",
-  "wordchain .",
-  "word-chain .",
-];
+export const prefixes = ["wc.", "wc/", "wordchain.", "word-chain."];
 
 export const secondsToJoin = 60;
 export const mediumTurnSeconds = [30, 25, 20];
