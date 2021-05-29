6059fda8f038bdf3c752085ae3df8e5a46f4a655
Mettle X <56449790+mettlex@users.noreply.github.com>
2021-05-29 16:34:47 +0600

    improve env setter

--- src/utils/check-env.ts
+++ src/utils/check-env.ts
@@ -1,15 +1,17 @@
 import fs from "fs";
 import path from "path";
 import dotenv from "dotenv";
+import { getAppRootDir } from "../app";
 
-export default function (): boolean {
-  const envFilePath = path.resolve(__dirname, "..", "..", ".env");
+export default function checkEnv(): boolean {
+  const envFilePath = path.resolve(getAppRootDir(), "..", ".env");
 
   if (fs.existsSync(envFilePath)) {
-    dotenv.config();
+    dotenv.config({ path: envFilePath });
   }
 
   if (!process.env.TOKEN) {
+    // eslint-disable-next-line no-console
     console.error("No access token found.");
     return false;
   }
