18c778511d89d43995d67bb4fbb7f9939adb6442
Mettle X <56449790+mettlex@users.noreply.github.com>
2021-06-15 03:16:13 +0600

    add start & join mechanism for coup without turn msg

---
+++ src/games/coup-reformation/game-loop.ts
@@ -0,0 +1,130 @@
+import { oneLine, stripIndents } from "common-tags";
+import { differenceInSeconds } from "date-fns";
+import { Message, MessageEmbed } from "discord.js";
+import { ButtonStyle, ComponentType } from "slash-create";
+import { flatColors } from "../../config";
+import { ExtendedTextChannel } from "../../extension";
+import { shuffleArray } from "../../utils/array";
+import { prefixes, timeToJoinInSeconds } from "./config";
+import {
+  createDeck,
+  getCurrentCoupReformationGame,
+  setCurrentCoupReformationGame,
+  setInitialMessageAndEmbed,
+} from "./data";
+
+export const askToJoinCoupGame = async (message: Message) => {
+  const embed = new MessageEmbed()
+    .setTitle("Join Coup Board Game!")
+    .setDescription(
+      stripIndents`${oneLine`_You are head of influences
+    in a city run by a weak and corrupt court. You need to manipulate,
+    bluff and bribe your way to power. Your object is to destroy
+    the influences of all the other players, forcing them into exile.
+    Only one player will survive..._`}
+
+    ${oneLine`In Coup, you want to be the last player with
+    influence in the game, with influence being represented
+    by hidden character cards in your playing area.`}
+
+    ${oneLine`You may join by pressing the button below
+    or by sending \`${prefixes[0]}join\``}
+  `,
+    )
+    .addField("Time left to join", `${timeToJoinInSeconds} seconds`, false)
+    .setFooter(
+      `${
+        message.member?.nickname || message.author.username
+      } is asking you to join`,
+    )
+    .setColor(flatColors.yellow);
+
+  const channel = message.channel as ExtendedTextChannel;
+
+  const initialMessage = (await channel.sendWithComponents({
+    content: "",
+    options: { embed },
+    components: [
+      {
+        components: [
+          {
+            type: ComponentType.BUTTON,
+            label: "Count me in!",
+            custom_id: "join_coup",
+            style: ButtonStyle.PRIMARY,
+          },
+        ],
+      },
+    ],
+  })) as Message;
+
+  const startTime = new Date();
+
+  const interval = setInterval(() => {
+    const game = getCurrentCoupReformationGame(message.channel.id);
+
+    if (!game) {
+      clearInterval(interval);
+      return;
+    }
+
+    const timeLeft =
+      timeToJoinInSeconds - differenceInSeconds(new Date(), startTime);
+
+    if (timeLeft <= 0 || game.gameStarted) {
+      embed.fields[0].name = `Time up!`;
+      embed.fields[0].value = `Let's see who joined below.`;
+
+      initialMessage.edit(embed);
+
+      startCoupGame(initialMessage);
+
+      clearInterval(interval);
+
+      return;
+    }
+
+    embed.fields[0].value = `${timeLeft} seconds`;
+
+    initialMessage.edit(embed);
+  }, 3000);
+
+  setInitialMessageAndEmbed({ message: initialMessage, embed, interval });
+};
+
+export const startCoupGame = (message: Message) => {
+  let game = getCurrentCoupReformationGame(message.channel.id);
+
+  if (!game) {
+    message.channel.send("> No initial game data found to start.");
+    return;
+  }
+
+  if (game.players.length < 2) {
+    message.channel.send("> At least 2 players are needed to start Coup game.");
+
+    setCurrentCoupReformationGame(message.channel.id, null);
+
+    return;
+  }
+
+  game = {
+    ...game,
+    deck: createDeck({
+      playersCount: game.players.length,
+      gameMode: "classic",
+    }),
+    gameStarted: true,
+    gameStartedAt: new Date(),
+  };
+
+  shuffleArray(game.players);
+
+  game.currentPlayer = game.players[0].id;
+
+  for (let i = 0; i < game.players.length; i++) {
+    game.players[i].influences = [game.deck.pop()!, game.deck.pop()!];
+  }
+
+  setCurrentCoupReformationGame(message.channel.id, game);
+};
--- src/games/coup-reformation/index.ts
+++ src/games/coup-reformation/index.ts
@@ -1,23 +1,35 @@
 import { oneLine } from "common-tags";
-import { Client } from "discord.js";
-import { SlashCreator } from "slash-create";
+import { Client, MessageEmbed } from "discord.js";
+import { ButtonStyle, ComponentType, SlashCreator } from "slash-create";
 import { setupGame } from "../setup";
 import { Action } from "../types";
 import { prefixes, timeToJoinInSeconds } from "./config";
 import { flatColors } from "../../config";
 import {
   getCurrentCoupReformationGame,
+  getDescriptionFromCardName,
   getInitialMessageAndEmbed,
+  influenceCardImagesClassic,
+  setCurrentCoupReformationGame,
 } from "./data";
 import { sendCoupHelpMessage } from "./handlers/help";
-import { ExtendedTextChannel } from "../../extension";
+import { ExtendedMessage, ExtendedTextChannel } from "../../extension";
 import { getGuildIds } from "../../app";
-import { makeCoupCommands } from "./slash-commands";
+import {
+  makeCoupCommands,
+  slashCommandOptionsForCheckCards,
+} from "./slash-commands";
+import { InfluenceCard } from "./types";
+import { askToJoinCoupGame, startCoupGame } from "./game-loop";
 
 export const actions: Action[] = [
   {
     commands: ["fs", "force-start", "force start"],
     handler: async (message) => {
+      if (message.author.bot || message.channel.type !== "text") {
+        return;
+      }
+
       if (!message.member?.hasPermission("MANAGE_GUILD")) {
         return;
       }
@@ -28,7 +40,8 @@ export const actions: Action[] = [
         const { message: initialMessage, embed, interval } = initialData;
 
         embed.setColor(flatColors.blue);
-        embed.setFooter(`0 seconds remaining.`);
+        embed.fields[0].name = `Time up!`;
+        embed.fields[0].value = `Let's see who joined below.`;
 
         try {
           await initialMessage.edit(embed);
@@ -42,9 +55,9 @@ export const actions: Action[] = [
       const game = getCurrentCoupReformationGame(message.channel.id);
 
       if (game && !game.gameStarted) {
-        // startCoupReformationGame(message);
+        startCoupGame(message);
       } else if (game && game.gameStarted) {
-        // changeCoupReformationTurn(message);
+        // changeCoupTurn(message);
       } else {
         message.reply(
           oneLine`there is no initiated game so
@@ -71,8 +84,8 @@ export const actions: Action[] = [
 
       channel
         .send({
-          content: oneLine`Use \`/check_cards_in_coup\` slash command to
-          check your influence cards secretly.`,
+          content: oneLine`Use \`/${slashCommandOptionsForCheckCards.name}\`
+          slash command to check your influence cards secretly.`,
         })
         .catch((e) => {
           // eslint-disable-next-line no-console
@@ -82,9 +95,146 @@ export const actions: Action[] = [
     description: "Check your own Influence Cards secretly.",
   },
   {
-    commands: [],
-    handler: () => {},
-    description: "Start a new Coup game.",
+    commands: ["allcards", "all cards"],
+    handler: async (message) => {
+      if (message.author.bot || message.channel.type !== "text") {
+        return;
+      }
+
+      const keys = Object.keys(influenceCardImagesClassic).sort();
+
+      const name = keys[0] as InfluenceCard["name"];
+
+      const embed = new MessageEmbed()
+        .setTitle(name.toUpperCase())
+        .setDescription(getDescriptionFromCardName(name))
+        .setImage(influenceCardImagesClassic[name][0]);
+
+      const channel = message.channel as ExtendedTextChannel;
+
+      channel.sendWithComponents({
+        content: "",
+        options: { embed },
+        components: [
+          {
+            components: [
+              {
+                label: "Previous",
+                custom_id: "previous_influence_card_1",
+                disabled: true,
+                type: ComponentType.BUTTON,
+                style: ButtonStyle.PRIMARY,
+              },
+              {
+                label: "Next",
+                custom_id: "next_influence_card_1",
+                disabled: false,
+                type: ComponentType.BUTTON,
+                style: ButtonStyle.PRIMARY,
+              },
+            ],
+          },
+        ],
+      });
+    },
+    description: "Show all available influence cards.",
+  },
+  {
+    commands: ["stop"],
+    handler: (message) => {
+      if (message.author.bot || message.channel.type !== "text") {
+        return;
+      }
+
+      if (!message.member?.hasPermission("MANAGE_GUILD")) {
+        message.reply(
+          oneLine`Only a member with Manage Server permission
+          can force-stop the game.`,
+        );
+        return;
+      }
+
+      const game = getCurrentCoupReformationGame(message.channel.id);
+
+      if (!game) {
+        message.reply("There is no Coup game running.");
+        return;
+      }
+
+      setCurrentCoupReformationGame(message.channel.id, null);
+
+      message.channel.send("> Successfully stopped the current Coup game.");
+    },
+  },
+  {
+    commands: ["s", "start", "begin"],
+    handler: (message) => {
+      if (message.author.bot) {
+        return;
+      }
+
+      let game = getCurrentCoupReformationGame(message.channel.id);
+
+      if (game) {
+        message.reply("There is already a Coup game running.");
+        return;
+      }
+
+      game = {
+        gameStarted: false,
+        gameStartedAt: new Date(),
+        mode: "classic",
+        deck: [],
+        players: [
+          {
+            id: message.author.id,
+            tag: message.author.tag,
+            coins: 2,
+            influences: [],
+          },
+        ],
+        currentPlayer: message.author.id,
+      };
+
+      setCurrentCoupReformationGame(message.channel.id, game);
+
+      askToJoinCoupGame(message).catch((e) => {
+        // eslint-disable-next-line no-console
+        console.error(e);
+      });
+    },
+    description: "Start a new Coup game in the current text channel.",
+  },
+  {
+    commands: ["j", "join"],
+    handler: (message) => {
+      const game = getCurrentCoupReformationGame(message.channel.id);
+
+      if (!game) {
+        message.reply("There is no Coup game running.");
+        return;
+      }
+
+      if (game.gameStarted) {
+        message.reply("The game started already so it's not joinable now.");
+        return;
+      }
+
+      if (game.players.find((p) => p.id === message.author.id)) {
+        message.reply("You already joined the game.");
+        return;
+      }
+
+      game.players.push({
+        id: message.author.id,
+        tag: message.author.tag,
+        coins: 2,
+        influences: [],
+      });
+
+      message.channel.send(`${message.author} joined the game.`);
+    },
+    description: "Join the Coup game",
   },
 ];
 
@@ -115,4 +265,67 @@ export const setupCoupReformationGame = (
       creator.syncCommands({ syncGuilds: true });
     }
   }, 3000);
+
+  creator.on("componentInteraction", async (ctx) => {
+    const nextCustomIdPartial = "next_influence_card_";
+    const previousCustomIdPartial = "previous_influence_card_";
+
+    const changeCardEmbed = async (i: number) => {
+      const channel = (await client.channels.fetch(
+        ctx.channelID,
+      )) as ExtendedTextChannel;
+
+      const message = (await channel.messages.fetch(
+        ctx.message.id,
+      )) as ExtendedMessage;
+
+      const keys = Object.keys(influenceCardImagesClassic).sort();
+
+      const name = keys[i] as InfluenceCard["name"];
+
+      const embed = new MessageEmbed()
+        .setTitle(name.toUpperCase())
+        .setDescription(getDescriptionFromCardName(name))
+        .setImage(influenceCardImagesClassic[name][0]);
+
+      message.editWithComponents({
+        content: "",
+        options: { embed },
+        components: [
+          {
+            components: [
+              {
+                label: "Previous",
+                custom_id: `previous_influence_card_${i - 1}`,
+                disabled: i === 0,
+                type: ComponentType.BUTTON,
+                style: ButtonStyle.PRIMARY,
+              },
+              {
+                label: "Next",
+                custom_id: `next_influence_card_${i + 1}`,
+                disabled: i === keys.length - 1,
+                type: ComponentType.BUTTON,
+                style: ButtonStyle.PRIMARY,
+              },
+            ],
+          },
+        ],
+      });
+    };
+
+    if (ctx.customID.startsWith(nextCustomIdPartial)) {
+      const i = parseInt(ctx.customID.replace(nextCustomIdPartial, ""));
+      changeCardEmbed(i).catch((e) => {
+        // eslint-disable-next-line no-console
+        console.error(e);
+      });
+    } else if (ctx.customID.startsWith(previousCustomIdPartial)) {
+      const i = parseInt(ctx.customID.replace(previousCustomIdPartial, ""));
+      changeCardEmbed(i).catch((e) => {
+        // eslint-disable-next-line no-console
+        console.error(e);
+      });
+    }
+  });
 };
